062b172d1aa234c32989ef552d0aa9c17a7ec076
Emese Kiss <emesekiss92@yahoo.com>
2021-01-17 18:39:06 +0100

    Correct file path for import

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,11 @@
-import EventList from './EventList';
-import EventForm from './EventForm';
+import EventList from './components/EventList';
+import EventForm from './components/EventForm';
 
 function App() {
   return (
     <div>
       <h1>Sportradar Challenge - Calendar</h1>
+      <EventForm />
       <EventList />
     </div>
   );
